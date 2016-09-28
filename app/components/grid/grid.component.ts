15f6f1bf8085170c538b8415d7273dbb109135d7
madthad91 <thaddeus.madison@gmail.com>
2016-09-27 22:13:02 -0500

    added css stuff

--- app/components/grid/grid.component.ts
+++ app/components/grid/grid.component.ts
@@ -9,26 +9,27 @@ import { Tile } from '../tile/tile';
 
 export class GridComponent implements OnInit {
 	tiles: Tile[];
+	childTitle:string = 'This text is passed to child';
 	constructor() { }
 	// Create empty board
 	ngOnInit(): void {
 		this.tiles = [
-			{x:0,y:0,color: "#FFF", val: 0},
-			{x:0,y:1,color: "#FFF", val: 0},
-			{x:0,y:2,color: "#FFF", val: 0},
-			{x:0,y:3,color: "#FFF", val: 0},
-			{x:1,y:0,color: "#FFF", val: 0},
-			{x:1,y:1,color: "#FFF", val: 0},
-			{x:1,y:2,color: "#FFF", val: 0},
-			{x:1,y:3,color: "#FFF", val: 0},
-			{x:2,y:0,color: "#FFF", val: 0},
-			{x:2,y:1,color: "#FFF", val: 0},
-			{x:2,y:2,color: "#FFF", val: 0},
-			{x:2,y:3,color: "#FFF", val: 0},
-			{x:3,y:0,color: "#FFF", val: 0},
-			{x:3,y:1,color: "#FFF", val: 0},
-			{x:3,y:2,color: "#FFF", val: 0},
-			{x:3,y:3,color: "#FFF", val: 0},
+			{x:0,y:0,color: "tile-2", val: 0},
+			{x:0,y:1,color: "tile-2", val: 0},
+			{x:0,y:2,color: "tile-4", val: 0},
+			{x:0,y:3,color: "tile-512", val: 0},
+			{x:1,y:0,color: "tile-512", val: 0},
+			{x:1,y:1,color: "tile-512", val: 0},
+			{x:1,y:2,color: "tile-512", val: 0},
+			{x:1,y:3,color: "tile-512", val: 0},
+			{x:2,y:0,color: "tile-512", val: 0},
+			{x:2,y:1,color: "tile-512", val: 0},
+			{x:2,y:2,color: "tile-512", val: 0},
+			{x:2,y:3,color: "tile-512", val: 0},
+			{x:3,y:0,color: "tile-512", val: 0},
+			{x:3,y:1,color: "tile-512", val: 0},
+			{x:3,y:2,color: "tile-512", val: 0},
+			{x:3,y:3,color: "tile-512", val: 0},
 		];
 	}
 }
--- app/components/tile/tile.component.ts
+++ app/components/tile/tile.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
 	moduleId: module.id,
 	selector: 'Tile',
-	templateUrl: 'tile.component.html'
+	templateUrl: 'tile.component.html',
+	styleUrls: ['tile.component.css']
 })
 
-export class TileComponent { }
+export class TileComponent { 
+	@Input() tileColor:string;
+}
