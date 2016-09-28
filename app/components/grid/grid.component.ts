eafb0cca4a0ef5e2a7724eb5218f1f9d8b1093f2
madthad91 <thaddeus.madison@gmail.com>
2016-09-27 22:31:25 -0500

    tile text enhancements

--- app/components/grid/grid.component.ts
+++ app/components/grid/grid.component.ts
@@ -14,22 +14,22 @@ export class GridComponent implements OnInit {
 	// Create empty board
 	ngOnInit(): void {
 		this.tiles = [
-			{x:0,y:0,color: "tile-2", val: 0},
-			{x:0,y:1,color: "tile-2", val: 0},
-			{x:0,y:2,color: "tile-4", val: 0},
-			{x:0,y:3,color: "tile-512", val: 0},
-			{x:1,y:0,color: "tile-512", val: 0},
-			{x:1,y:1,color: "tile-512", val: 0},
-			{x:1,y:2,color: "tile-512", val: 0},
-			{x:1,y:3,color: "tile-512", val: 0},
-			{x:2,y:0,color: "tile-512", val: 0},
-			{x:2,y:1,color: "tile-512", val: 0},
-			{x:2,y:2,color: "tile-512", val: 0},
-			{x:2,y:3,color: "tile-512", val: 0},
-			{x:3,y:0,color: "tile-512", val: 0},
-			{x:3,y:1,color: "tile-512", val: 0},
-			{x:3,y:2,color: "tile-512", val: 0},
-			{x:3,y:3,color: "tile-512", val: 0},
+			{x:0,y:0,color: "tile-2", val: 2},
+			{x:0,y:1,color: "tile-2", val: 2},
+			{x:0,y:2,color: "tile-4", val: 4},
+			{x:0,y:3,color: "tile-512", val: 512},
+			{x:1,y:0,color: "tile-512", val: 512},
+			{x:1,y:1,color: "tile-512", val: 512},
+			{x:1,y:2,color: "tile-512", val: 512},
+			{x:1,y:3,color: "tile-512", val: 512},
+			{x:2,y:0,color: "tile-512", val: 512},
+			{x:2,y:1,color: "tile-512", val: 512},
+			{x:2,y:2,color: "tile-512", val: 512},
+			{x:2,y:3,color: "tile-512", val: 512},
+			{x:3,y:0,color: "tile-512", val: 512},
+			{x:3,y:1,color: "tile-512", val: 512},
+			{x:3,y:2,color: "tile-512", val: 512},
+			{x:3,y:3,color: "tile-512", val: 512},
 		];
 	}
 }
